359d8b9eed2506e6cc3aaf1bb96ccf425cd3bfd1
23DP4ASemj <166485080+23DP4ASemj@users.noreply.github.com>
2024-09-12 11:51:17 +0000

    hello world

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        System.err.println("Hello, world!");
+        
     }
 }
+            
