bc3bf7962866f9a3f77dfba9bb9fbf2795139564
23DP4ASemj <166485080+23DP4ASemj@users.noreply.github.com>
2024-10-10 12:02:47 +0000

    faktorial

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,17 @@
 package lv.rvt;
 
-public class App 
+import java.util.*;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.err.println("Hello, world!");
-        
+public static void main( String[] args )
+{
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give a number: ");
+
+    
+        for ( int Num = Integer.valueOf(scanner.nextLine()); Num < 101; Num++) {
+            System.out.println(Num);
+        }
     }
 }
-            
