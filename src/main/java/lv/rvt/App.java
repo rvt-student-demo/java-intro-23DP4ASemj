f8a9004afb899f464538504ad9461978fd3b5000
23DP4ASemj <166485080+23DP4ASemj@users.noreply.github.com>
2024-10-11 10:46:05 +0000

    more_loops

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,41 @@ public class App
 {
 public static void main( String[] args )
 {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Give a number: ");
-    long fact = 1;
-    
-    int Num = Integer.valueOf(scanner.nextLine());
-    for ( int i = 1; i <= Num; i++){
-        fact *=i;
-    }
+    Scanner reader = new Scanner(System.in);
 
+    System.out.print("Write numbers:  ");
+    int sum = 0;
+    int Numbers = 0;
+    int average = 0;
+    int odd = 0;
+    int even = 0;
+    while (true) {
+        int input = Integer.valueOf(reader.nextLine());
+
+        if (input == 0) {
+            break;
+        }
+       else {
+            Numbers++;
+        }
+        if (input % 2 == 0) {
+            odd++;
+        }
+        else {
+            even++;
+        }
         
-         System.out.println(fact);
+        average = sum/Numbers;
+        sum += input;
+        Numbers++;
     }
+    System.out.println("Thx! Bye!");
+    System.out.println("Sum: " + sum);
+    System.out.println("Numbers: " + Numbers);
+    System.out.println("Average: " + average);
+    System.out.println("Even: " + even);
+    System.out.println("Odd: " + odd);
+
+
+} 
 }
