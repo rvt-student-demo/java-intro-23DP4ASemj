94c199fd4150aee77c96fbc0f7241d7b69963500
23DP4ASemj <166485080+23DP4ASemj@users.noreply.github.com>
2024-10-31 13:23:52 +0000

    Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,23 @@
 package lv.rvt;
 
-    public class App{
-        class Exercise1
-        {
-          public static void main ( String[] args )
-          {
-            int[] val = {0, 1, 2, 3}; 
-        
-            int sum = val [0] + val [1] + val [2] + val [3];
-         
-            System.out.println( "Sum of all numbers = " + sum );
-         
-           }
-        }
+public class App{
+    public class Main {
+        public static void main(String[] args) {
+
+           
+            int[] val = {13, -4, 82, 17};
+            int[] twice = new int[4];
+
+            System.out.println();
+            System.out.println();
+            System.out.println("Orig. mas.: " + val[0]+ val[1]+ val[2]+ val[3]);
+
+            for (int i = 0; i < val.length; i++){
+                twice[i] = val[i] * 2;
+
+            }
+
+            System.out.println( "New Array: "  + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+}
+}
 }
